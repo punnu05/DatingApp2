65b81a51dfe3400f20a5a4028c18c62f0289f279
Punnu Chavan <Punnu.Chavan@esds.co.in>
2023-01-19 17:27:14 +0530

    second section done

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.AppUsers.ToListAsync();
+            return users;
+        }
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int Id)
+        {
+            return await _context.AppUsers.FindAsync(Id);
+        }
+        [HttpPost]
+        public async Task<string> PostUser(AppUser user)
+        {
+            try
+            {
+                await _context.AppUsers.AddAsync(user);
+                _context.SaveChangesAsync();
+                return "added";
+            }
+            catch
+            {
+                return "failed to add";
+            }
+        }
+    }
+}

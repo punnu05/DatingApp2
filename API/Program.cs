009872a311386833defc33d2069743b3175d61a4
Punnu Chavan <Punnu.Chavan@esds.co.in>
2023-01-20 17:50:39 +0530

    End of 3rd section

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
 opt.UseSqlite(builder.Configuration.GetConnectionString("Default"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 
-
+app.UseCors(builder =>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 
 
---
+++ ClientDApp/src/app/Models/user.ts
@@ -0,0 +1,4 @@
+export class user{
+    Id!:number;
+    UserName? :string;
+}
